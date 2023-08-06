aa59de59e6a1cf6b515f34e41c7fec5c01f76e0d
unknown <alvinjoyner3@gmail.com>
2023-08-05 22:00:57 -0400

    running intop node 18 version problems going to psuh for now and try to install node

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,28 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+console.log('install.js working');
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+  window.deferredPrompt = event;
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+  butInstall.classList.toggle('hidden', false);
+});
+
+butInstall.addEventListener('click', async () => {
+  const promptEvent = window.deferredPrompt;
+  console.log('I have been clicked');
+  if (!promptEvent) {
+    return;
+  }
+
+  promptEvent.prompt();
+
+  window.deferredPrompt = null;
+
+  butInstall.classList.toggle('hidden', true);
+  console.log('Working');
+});
+
+window.addEventListener('appinstalled', (event) => {
+  window.deferredPrompt = null;
+});
